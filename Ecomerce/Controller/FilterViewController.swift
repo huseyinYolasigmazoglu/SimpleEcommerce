1441aa583f18d375923059cc39fe3eeaaa446d23
huseyinYolasigmazoglu <huseyinyolasigmazoglu@hotmail.com>
2020-12-27 13:25:41 +0300

    added emty filter page for later implementation

---
+++ Ecomerce/Controller/FilterViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FilterViewController.swift
+//  Ecomerce
+//
+//  Created by Huseyin Yolasigmazoglu on 27.12.2020.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+    
+    
+    @IBAction func closeScreen(_ sender: UIBarButtonItem) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
